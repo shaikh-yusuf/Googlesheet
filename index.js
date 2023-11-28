eeb662baaf5f319caa89522cdbe86d8c9c8cb0f7
shaikh <shaikh8484047332@gmail.com>
2023-11-28 09:48:01 +0530

    this basic changes

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
-const header = document.getElementById("header");
-const body = document.getElementById("body");
+let columns = 26;
+let rows = 100;
+const headerContainer = document.querySelector(".header");
+const serialNumbersContainer = document.querySelector(".sno");
+const mainContainer = document.querySelector(".main");
+// A => 65
 
-for (let i = 65; i <= 90; i++) {
-  let char = String.fromCharCode(i);
-
-  const bold = document.createElement("b");
-  bold.innerText = char;
-
-  header.appendChild(bold);
-}
-
-function createAndAppendRow(rowNumber) {
-  const row = document.createElement("div");
-  row.className = "row"; // <div class="row"></div>
-  // inside each row we should create 27 cells one for S.No
-  for (let i = 64; i <= 90; i++) {
-    if (i === 64) {
-      // This cell represents the S.No
-      const b = document.createElement("b");
-      b.innerText = rowNumber;
-      row.appendChild(b);
-    } else {
-      // This represents the empty cell
-      const cell = document.createElement("div");
-      cell.id = `${String.fromCharCode(i)}${rowNumber}`; // dynamic and unique id. COLROW => examples: C7, M8, A3
-      cell.contentEditable = "true";
-      cell.addEventListener("focus", onCellFocus);
-      row.appendChild(cell);
+function createHeaderCells() {
+    for (let i = 0; i <= columns; i++) {
+        const headerCell = document.createElement("div");
+        headerCell.className = "header-cell cell";
+        if (i !== 0) {
+            // i = 1 => Char A => 64 + 1 => 65
+            headerCell.innerText = String.fromCharCode(64 + i);
+        }
+        headerContainer.appendChild(headerCell)
     }
-  }
-  body.appendChild(row);
 }
 
-// create 100 rows call the `createAndAppendRow` function for 100 times.
-
-for (let i = 1; i <= 100; i++) {
-  createAndAppendRow(i);
+function createSerialNumberCells() {
+    for (let i = 1; i <= rows; i++) {
+        const snoCell = document.createElement("div");
+        snoCell.innerText = i;
+        snoCell.className = "sno-cell cell"
+        serialNumbersContainer.appendChild(snoCell);
+    }
 }
+
+function createRow(rowNumber) {
+    // creates a row and each row will have 26 columns
+    const row = document.createElement("div");
+    row.className = "row";
+    for (let i = 1; i <= columns; i++) {
+        const cell = document.createElement("div");
+        cell.className = "main-cell cell";
+        cell.contentEditable = true;
+        row.appendChild(cell);
+
+        cell.id = String.fromCharCode(64 + i) + rowNumber;
+
+        // adding focus event for every cell.
+        cell.addEventListener("focus", onCellFocus);
+        // add input event as we need to record the inside the input 
+        cell.addEventListener("input", onFormChange);
+    }
+    mainContainer.appendChild(row);
+}
+
+function buildMainSection() {
+    // loop for 100 times
+    for (let i = 1; i <= rows; i++) {
+        createRow(i);
+    }
+}
+
+createHeaderCells();
+createSerialNumberCells();
+
+buildMainSection();
--- options.js
+++ options.js
@@ -1,172 +1,98 @@
-// we manage the options selection
+const cellNamePlaceholder = document.querySelector("#active-cell");
+const fontSizeInput = document.querySelector("#fontsize");
+const fontFamilyInput = document.querySelector("#fontfamily");
+const form = document.querySelector("#form")
 
-const activeCellElement = document.getElementById("active-cell");
-const textAlignElements = document.getElementsByClassName("text-align");
-const boldButton = document.getElementById("bold");
-const italicButton = document.getElementById("italic");
-const underlinedButton = document.getElementById("underlined");
+let activeElement = null;
 
-// activeCell defines which cell is selected / active.
-// intially it will null indicating that no cell is selected.
-let activeCell = null;
+const state = {};
 
-let activeOptionsState;
-
-function toggleButtonsStyle(button, isSelected) {
-  if (isSelected) {
-    // currently selected cell in the bold state.
-    button.classList.add("active-option");
-  } else {
-    button.classList.remove("active-option");
-  }
+const defaultProperties = {
+    fontFamily: 'sans',
+    fontSize: 16,
+    color: "#000000",
+    textAlign: "left", // "left", "center", "right"
+    backgroundColor: "#ffffff",
+    isBold: false,
+    isItalic: false,
+    isUnderlined: false,
+    value: ''
 }
 
-function highlightOptionButtonsOnFocus() {
-  // check if the cell is in the bold state or not .
-  // if (activeOptionsState.isBoldSelected) {
-  //   // currently selected cell in the bold state.
-  //   boldButton.classList.add("active-option");
-  // } else {
-  //   boldButton.classList.remove("active-option");
-  // }
-
-  toggleButtonsStyle(boldButton, activeOptionsState.isBoldSelected);
-
-  // check if the selected cell is italic or not .
-  // if (activeOptionsState.isItalicSelected) {
-  //   // the current cell is italic text.
-  //   italicButton.classList.add("active-option");
-  // } else {
-  //   italicButton.classList.remove("active-option");
-  // }
-  toggleButtonsStyle(italicButton, activeOptionsState.isItalicSelected);
-
-  // check if the selected cell is underline or not .
-
-  // if (activeOptionsState.isUnderLineSelected) {
-  //   // the cell is underlined
-  //   underlinedButton.classList.add("active-option");
-  // } else {
-  //   underlinedButton.classList.remove("active-option");
-  // }
-  toggleButtonsStyle(underlinedButton, activeOptionsState.isUnderLineSelected);
-
-  // get the textAlign value
-  highlightTextAlignButtons(activeOptionsState.textAlign);
-  // highlightTextAlignButtons("start" | "right" | "center")
-}
-
-// below function will be triggered whenever cell is focused.
-function onCellFocus(e) {
-  // whenever a cell is focused change the activeCell value to be the id of cell.
-  if (activeCell && activeCell.id === e.target.id) {
-    // previously selected cell is equal to the currently selected cell.
-    return;
-  }
-  activeCell = e.target;
-  activeCellElement.innerText = e.target.id;
-  // intialise the state of this cell.
-  const computedStyle = getComputedStyle(activeCell);
-  activeOptionsState = {
-    fontFamily: computedStyle.fontFamily,
-    isBoldSelected: computedStyle.fontWeight === "600",
-    isItalicSelected: computedStyle.fontStyle === "italic",
-    isUnderLineSelected: computedStyle.textDecoration.includes("underline"),
-    textAlign: computedStyle.textAlign,
-    textColor: computedStyle.color,
-    backgroundColor: computedStyle.backgroundColor,
-    fontSize: computedStyle.fontSize,
-  };
-
-  highlightOptionButtonsOnFocus();
-}
-
-function onClickBold(boldButton) {
-  // this function will be triggered when user clicks on the Bold button.
-  /**
-   * 1. toggle `active-option` class for the button.
-   * 2. get the selected cell.
-   */
-  boldButton.classList.toggle("active-option");
-  if (activeCell) {
-    if (activeOptionsState.isBoldSelected === false) {
-      // make the text to bold
-      activeCell.style.fontWeight = "600";
-    } else {
-      // make the text to normal
-      activeCell.style.fontWeight = "400";
+function onCellFocus(event) {
+    const elementId = event.target.id;
+    cellNamePlaceholder.innerText = elementId;
+    activeElement = event.target;
+    if (state[elementId]) {
+        // already selected cell
+        // fill the options with the state of that cell
+        resetOptions(state[elementId]);
     }
-    activeOptionsState.isBoldSelected = !activeOptionsState.isBoldSelected;
-  }
-}
-
-function onClickItalic(italicButton) {
-  /**
-   * 1. toggle the active-option class for the italic button.
-   */
-  italicButton.classList.toggle("active-option");
-  if (activeCell) {
-    if (activeOptionsState.isItalicSelected) {
-      // the text already italic.
-      activeCell.style.fontStyle = "normal";
-    } else {
-      activeCell.style.fontStyle = "italic";
+    else {
+        // selected for the first time
+        // fill the options with default state
+        resetOptions(defaultProperties);
     }
-    activeOptionsState.isItalicSelected = !activeOptionsState.isItalicSelected;
-  }
 }
 
-function onClickUnderline(underlinedButton) {
-  underlinedButton.classList.toggle("active-option");
-  if (activeCell) {
-    if (activeOptionsState.isUnderLineSelected) {
-      // if the text is underlined => none
-      activeCell.style.textDecoration = "none";
-    } else {
-      activeCell.style.textDecoration = "underline";
+function resetOptions(optionsState) {
+    // updates the UI as per the optionsState
+    /**
+     * optionsState = {
+     *    fontSize: ,
+     *    fontFamily: ,
+     *    color: '',
+     *    textAlign: ''
+     * }
+     */
+    form.fontfamily.value = optionsState.fontFamily;
+    form.fontsize.value = optionsState.fontSize;
+    form.textalign.value = optionsState.textAlign; // "right" | "left" | "center"
+    form.bold.checked = optionsState.isBold
+    form.italic.checked = optionsState.isItalic;
+    form.underlined.checked = optionsState.isUnderlined;
+    form.textcolor.value = optionsState.color;
+    form.bgcolor.value = optionsState.backgroundColor;
+}
+
+function onFormChange() {
+    if (!activeElement) {
+        alert("Please select a cell to make changes");
+        form.reset();
+        return;
     }
-    activeOptionsState.isUnderLineSelected =
-      !activeOptionsState.isUnderLineSelected;
-  }
-}
 
-// the below function task is to take the textAlign value and decides which alignment button needs to highlighted or not.
-function highlightTextAlignButtons(textAlignValue) {
-  // textAlignValue === "left" => we have to highlight only left button
-  // textAlignValue === "right" => we have to highlight only right button
-  // textAlignValue === "center" => we have to highlight only center button
-  for (let i = 0; i < textAlignElements.length; i++) {
-    if (textAlignElements[i].getAttribute("data-value") === textAlignValue) {
-      textAlignElements[i].classList.add("active-option");
-    } else {
-      textAlignElements[i].classList.remove("active-option");
+
+    let currentState = {
+        textColor: form.textcolor.value,
+        backgroundColor: form.bgcolor.value,
+        fontSize: form.fontsize.value,
+        fontFamily: form.fontfamily.value,
+        isBold: form.bold.checked,
+        isItalic: form.italic.checked,
+        isUnderlined: form.underlined.checked,
+        textAlign: form.textalign.value // "left" , "right" , "center"
     }
-  }
+
+    // below function applies all the styles to the active cell.
+    applyStylesToCell(currentState);
+
+    // update the state object for the current cell.
+    // state = {} 
+    // state["C2"] = currentState ;
+    // state = { C2: currentState }
+    state[activeElement.id] = { ...currentState, value: activeElement.innerText };
 }
 
-function onClickTextAlign(textAlignButton) {
-  let selectedValue = textAlignButton.getAttribute("data-value");
-  highlightTextAlignButtons(selectedValue);
+function applyStylesToCell(styleObject) {
+    // it will take the style object and applies it to the currently selected cell.
+    activeElement.style.fontSize = `${styleObject.fontSize}px`;
+    activeElement.style.fontFamily = styleObject.fontFamily;
+    activeElement.style.color = styleObject.textColor;
+    activeElement.style.backgroundColor = styleObject.backgroundColor;
+    activeElement.style.textAlign = styleObject.textAlign;
 
-  // change the text alignment.
-  if (activeCell) {
-    activeCell.style.textAlign = selectedValue;
-    activeOptionsState.textAlign = selectedValue;
-  }
-}
-
-function onChangeTextColor(textColorInput) {
-  let selectedColor = textColorInput.value;
-  if (activeCell) {
-    activeCell.style.color = selectedColor;
-    activeOptionsState.color = selectedColor;
-  }
-}
-
-function onChangeBackgroundColor(textColorInput) {
-  let selectedColor = textColorInput.value;
-  if (activeCell) {
-    activeCell.style.backgroundColor = selectedColor;
-    activeOptionsState.backgroundColor = selectedColor;
-  }
+    activeElement.style.fontWeight = styleObject.isBold ? "bold" : "normal";
+    activeElement.style.fontStyle = styleObject.isItalic ? "italic" : "normal";
+    activeElement.style.textDecoration = styleObject.isUnderlined ? "underline" : "none";
 }
